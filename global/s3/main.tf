7541c76903cffb0c00ff7ac44e9115387529698e
Petri Miikki <miikinpetri@gmail.com>
2020-03-16 12:25:14 +0200

    - Use modules for better handling multiple environments - Changed project structure to use 'best practices' by defining components in their own modules

--- global/s3/main.tf
+++ global/s3/main.tf
@@ -38,21 +38,21 @@ resource "aws_dynamodb_table" "terraform_locks" {
   }
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = terraform.workspace == "default" ? "t2.micro" : "t2.micro"
-}
+# resource "aws_instance" "example" {
+#   ami           = "ami-0c55b159cbfafe1f0"
+#   instance_type = terraform.workspace == "default" ? "t2.micro" : "t2.micro"
+# }
 
 
 # Partial configuration. The other settings (e.g. bucket, region) will be passed in from a file
 # via -backend-config arguments to 'terraform init'
-terraform {
-  backend "s3" {
-    bucket = "terraform-up-and-running-state-petsuri"
-    key    = "workspaces-example/terraform.tfstate"
-    region = "us-east-2"
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-up-and-running-state-petsuri"
+#     key    = "workspaces-example/terraform.tfstate"
+#     region = "us-east-2"
 
-    dynamodb_table = "terraform-up-and-running-locks-petsuri"
-    encrypt        = true
-  }
-}
+#     dynamodb_table = "terraform-up-and-running-locks-petsuri"
+#     encrypt        = true
+#   }
+# }
